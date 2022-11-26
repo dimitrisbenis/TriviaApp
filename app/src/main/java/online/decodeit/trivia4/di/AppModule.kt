6129b6c4a3bef7d30df1f19dd7c2e9c0ae423747
benis <dimitrios.benis@gmail.com>
2022-11-26 14:04:44 +0200

    Adding our ViewModels

--- app/src/main/java/online/decodeit/trivia4/di/AppModule.kt
+++ app/src/main/java/online/decodeit/trivia4/di/AppModule.kt
@@ -4,7 +4,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import online.decodeit.trivia4.network.CategoryAPI
 import online.decodeit.trivia4.network.QuestionAPI
+import online.decodeit.trivia4.repository.CategoryRepository
+import online.decodeit.trivia4.repository.QuestionRepository
 import online.decodeit.trivia4.utilities.Constants
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,13 +16,32 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     @Singleton
     @Provides
-    fun provideTriviaApi(): QuestionAPI {
+    fun provideCategoryRepository(api: CategoryAPI) = CategoryRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionAPI) = QuestionRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideTriviaQuestionsApi(): QuestionAPI {
         return Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(QuestionAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTriviaCategoriesApi(): CategoryAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CategoryAPI::class.java)
     }
 }
---
+++ app/src/main/java/online/decodeit/trivia4/screens/QuestionsViewModel.kt
@@ -0,0 +1,31 @@
+package online.decodeit.trivia4.screens
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import online.decodeit.trivia4.data.DataOrException
+import online.decodeit.trivia4.model.QuestionItem
+import online.decodeit.trivia4.repository.QuestionRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class QuestionsViewModel @Inject constructor(private val repository: QuestionRepository) : ViewModel() {
+    private val data: MutableState<DataOrException<ArrayList<QuestionItem>, Boolean, Exception>> =
+        mutableStateOf(DataOrException(null, true, Exception("")))
+
+    init {
+        getQuestions()
+    }
+
+    private fun getQuestions() {
+        viewModelScope.launch {
+            data.value.loading = true
+            data.value = repository.getQuestions()
+            if (data.value.data.toString().isNotEmpty())
+                data.value.loading = false
+        }
+    }
+}
